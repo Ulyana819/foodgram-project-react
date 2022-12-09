1918aa64b354ba6a20fb05adb9fc4ad2bf71a02d
git config --global user.name <shmakovauljana@yandex.ru>
2022-12-10 00:31:33 +0300

    v2

--- backend/api/views/users.py
+++ backend/api/views/users.py
@@ -6,7 +6,6 @@ from rest_framework.decorators import action
 from rest_framework.permissions import (IsAuthenticated,
                                         IsAuthenticatedOrReadOnly)
 from rest_framework.response import Response
-
 from users.models import User
 
 
