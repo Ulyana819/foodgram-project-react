01fb580de144023680247a8e1ea51597b472ddbb
git config --global user.name <shmakovauljana@yandex.ru>
2023-01-13 13:40:15 +0300

    v3

--- backend/api/views/recipes.py
+++ backend/api/views/recipes.py
@@ -1,9 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404
 from django_filters.rest_framework import DjangoFilterBackend
-from reportlab.lib.pagesizes import A4
-from reportlab.pdfbase import pdfmetrics
-from reportlab.pdfbase.ttfonts import TTFont
+from reportlab.pdfbase import pdfmetrics, ttfonts
 from reportlab.pdfgen import canvas
 from rest_framework import status, viewsets
 from rest_framework.decorators import action
